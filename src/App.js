0ccb5b6b26d3c5450a2e5bfdff4f97c3048147b7
Omar Muhanna <omarmuhomar@hotmail.com>
2020-05-07 19:41:12 -0500

    css/html stuff

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import Navbar from './Components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar/>
     <Switch>
        <Route exact path='/' component={LandingPage} />
        <Route path='/SignInPage' component={SignInPage} />
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,7 +5,21 @@ export default class Navbar extends Component {
     render() {
         return (
             <div>
-                <nav>TBM</nav>
+               <nav role='navigation'>
+      <ul className='basic-nav'>
+        <li><a href="">Order History</a></li>
+        <li><a href="">Sign Out</a></li>
+        <li><a href="">Cart</a></li>
+      </ul>
+      <h1 class="logo"><a href="">TBM</a></h1>
+      <input type="text" placeholder="search"/>
+      <br/>
+      <ul class="categories">
+        <li><a href="">Category 1</a></li>
+        <li><a href="">Category 2</a></li>
+        <li><a href="">Category 3</a></li>
+      </ul>
+    </nav>
             </div>
         )
     }
