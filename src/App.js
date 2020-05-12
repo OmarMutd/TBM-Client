f80a4ff355a3e55646c559a502afbc5494f2690c
Sean M <53903852+shmccoy@users.noreply.github.com>
2020-05-12 18:03:18 -0400

    Added Search bar and some CSS

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ import Navbar from './Components/Navbar/Navbar';
 
 
 
-
 function App() {
+
   return (
     <div className="App">
       <Navbar/>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,31 +1,64 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import "./Navbar.css"
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon }  from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/fontawesome-free-solid'
 
-export default class Navbar extends Component {
-  
-    render() {
-        return (
-          <div>
-            <nav role='navigation'>
-              <ul className='basic-nav'>
-                <li><Link to='/OrderHistory'>Order History</Link></li>
-                <li><Link to='/'>Sign Out</Link></li>
-                <li><Link to='/Cart'>Cart</Link></li>
-              </ul>
-              <h1 className="logo"><Link to='/Products'>TBM</Link></h1>
-              <input type="text" placeholder="search"/>
-              <br/>
-              <ul className="categories">
-                <li><a href="www.google.com">Animals</a></li>
-                <li><a href="www.google.com">Vehicles</a></li>
-                <li><a href="www.google.com">Clothing</a></li>
-                <li><a href="www.google.com">Furniture</a></li>
-              </ul>
-            </nav>
-          </div>
-        )
+import Navlinks from './Navlinks'
+import Lead from './Lead'
+
+import './Navbar.css'
+
+class Navbar extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      menu_class: '',
     }
-}
+  }
+
+  setToggleNavbarClass = () => {
+    if (this.state.menu_class === '') {
+      this.setState({
+        menu_class: 'toggled',
+      })
+    } else {
+      this.setState({
+        menu_class: '',
+      })
+    }
+  }
 
 
+  render = () => {
+    let nav_bar_class = `nav-bar ${this.state.menu_class}`
+    return (
+        <div>
+          <div className={nav_bar_class} >
+            <Lead text="The Black Market" />
+            <section className='left'>
+            <Link to=''><Navlinks text='Animals'/></Link>
+            <Link to=''><Navlinks text='Vehicles'/></Link>
+            <Link to=''><Navlinks text='Furniture'/></Link>
+            <Link to=''><Navlinks text='Households'/></Link>
+            </section>
+            
+            <section className='right'>
+              <form>
+              <input type="text" placeholder="Search.." name="search"/>
+              <button type="submit">Submit</button>
+            </form>
+            <Link to='./OrderHistory'><Navlinks text='Order History' /></Link>
+            <Link to='./'><Navlinks text='Sign Out' /></Link>
+              <Link to='./Cart'><Navlinks text='Cart' /></Link>
+            </section>
+            <FontAwesomeIcon icon={faBars} className='nav-bar-icon' onClick={this.setToggleNavbarClass}/>
+            <div className='clear-fix' />
+          </div>
+          </div>
+        
+    )
+  }
+  }
+
+export default Navbar;
