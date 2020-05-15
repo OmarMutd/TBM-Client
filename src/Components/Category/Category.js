69bdfc33ceb7eb855acc2ed61b8f152d1851f001
Omar Muhanna <omarmuhomar@hotmail.com>
2020-05-14 19:09:02 -0500

    fixed bug where category did not update automatically by implementing componentDidUpdate

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
-import  config from '../../config';
+import config from '../../config';
 import Item from '../Item/Item';
 
 
 
 export class Category extends Component {
-    state ={
+    state = {
         data: [],
         category: ''
     };
-    componentDidMount() {
-        const category = this.props.match.params.category;
-        this.setState({category: category})
-        console.log(category)
+
+    fetchByCategory = (category) => {
+
         fetch(`${config.API_ENDPOINT}/products/category/${category}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
             },
         })
-        .then(response => response.json())
-        .then(data =>  {this.setState({ data: data })});
+            .then(response => response.json())
+            .then(data => { this.setState({ data: data }) });
+    }
+    componentDidMount() {
+        const category = this.props.match.params.category;
+        this.setState({ category: category })
+        this.fetchByCategory(category)
+    }
+
+    componentDidUpdate(prevProps) {
+        const category = this.props.match.params.category;
+        if (category !== prevProps.match.params.category) {
+            this.fetchByCategory(category);
+            this.setState({ category: category });
         }
+    }
+
     render() {
         const value = this.state.data;
         return (
@@ -29,12 +42,12 @@ export class Category extends Component {
                 {/* Change category to plural and first letter capital */}
                 <h2>{this.state.category}</h2>
                 <div className='all-products'>
-                        <div className='products'>
-                             {value.map(item => {
-                              return <Item product={item} key={item.id} />;
-                            })}
-                        </div>
+                    <div className='products'>
+                        {value.map(item => {
+                            return <Item product={item} key={item.id} />;
+                        })}
                     </div>
+                </div>
             </div>
         )
     }
