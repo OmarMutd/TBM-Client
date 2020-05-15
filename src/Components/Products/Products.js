993577fa421ca5b7ca038916c5f5f9c648b37695
Sean M <53903852+shmccoy@users.noreply.github.com>
2020-05-14 20:41:06 -0400

    Added grid styling

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,44 +1,46 @@
-import React, { Component } from 'react';
-import './Products.css';
-import { ProductConsumer } from '../../context';
-import Item from '../Item/Item';
-import  config from '../../config';
+import React, { Component } from "react";
+import "./Products.css";
+import { ProductConsumer } from "../../context";
+import Item from "../Item/Item";
+import config from "../../config";
 
 export class Products extends Component {
-    state = {
-        data: [],
-    };
-    componentDidMount() {
-        fetch(`${config.API_ENDPOINT}/products`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(response => response.json())
-        .then(data => {this.setState({ data: data })});
-    };
+  state = {
+    data: [],
+  };
+  componentDidMount() {
+    fetch(`${config.API_ENDPOINT}/products`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ data: data });
+      });
+  }
 
-    render() {
-        const value = this.state.data;
-        console.log(value);
-        return (
-            <div>
-                <section>
-                    <h2>All Products</h2>
-                </section>
-                <section>
-                    <div className='all-products'>
-                        <div className='products'>
-                             {value.map(item => {
-                              return <Item product={item} key={item.id} />;
-                            })}
-                        </div>
-                    </div>
-                </section>
+  render() {
+    const value = this.state.data;
+    console.log(value);
+    return (
+      <div>
+        <section>
+          <h2>All Products</h2>
+        </section>
+        <section>
+          <div className="all-products">
+            <div className="products">
+              {value.map((item) => {
+                return <Item product={item} key={item.id} />;
+              })}
             </div>
-        );
-    }
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
-export default Products
+export default Products;
