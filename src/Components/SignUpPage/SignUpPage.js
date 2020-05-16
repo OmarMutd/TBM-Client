3e82f93662e2f16c88dee88730d9ef31f59cb1a5
Laura Spieker <ljspiek@gmail.com>
2020-05-16 13:18:46 -0500

    fixed routing for auto signin when navigating from sign in page

--- src/Components/SignUpPage/SignUpPage.js
+++ src/Components/SignUpPage/SignUpPage.js
@@ -6,19 +6,22 @@ import AuthApiService from "../../services/auth-api-services";
 import LoginContext from "../../LoginContext";
 import TokenService from "../../services/token-services";
 
-
 class SignUpPage extends Component {
   static contextType = LoginContext;
 
   state = {
     error: null,
+    fromSignIn: false,
   };
 
   handleSignInSuccess = () => {
     this.context.updateLogIn();
     this.setState({});
-
-    this.props.history.goBack();
+    if (this.state.fromSignIn === true) {
+      this.props.history.go(-2);
+    } else {
+      this.props.history.goBack();
+    }
   };
 
   handleSubmit = (e) => {
@@ -29,20 +32,16 @@ class SignUpPage extends Component {
 
     this.setState({ error: null });
 
-    
     AuthApiService.postUser({
       username: username,
       password: password,
     })
       .then(() => {
-
         AuthApiService.postLogin({
           username: username,
           password: password,
         }).then((res) => {
-          console.log("AUTH TOKEN", res.authToken);
           TokenService.saveAuthToken(res.authToken);
-
 
           this.handleSignInSuccess();
         });
@@ -52,49 +51,61 @@ class SignUpPage extends Component {
       });
   };
 
-  render() {
+  componentDidMount() {
+    if (this.props.location.state) {
+      this.setState({
+        fromSignIn: this.props.location.state.fromSignIn,
+      });
+    }
+  }
 
-    const { error } = this.state
+  render() {
+    const { error } = this.state;
     return (
       <div>
-        <div role="alert">
-          {error && <p className="sign-error">{error}</p>}
-        </div>
-        <form className='signup-form' onSubmit={this.handleSubmit}>
+        <div role="alert">{error && <p className="sign-error">{error}</p>}</div>
+        <form className="signup-form" onSubmit={this.handleSubmit}>
           <h2 className="signup-header">Create An Account</h2>
 
           <div className="username-signup">
-            <label htmlFor='username-input'></label>
+            <label htmlFor="username-input"></label>
             <input
-              id='username-input'
-              placeholder='Username'
-              className='input-field'
-              type='text'
-
+              id="username-input"
+              placeholder="Username"
+              className="input-field"
+              type="text"
               required
             />
           </div>
           <div className="password-signup">
-
-            <label htmlFor='password-input'></label>
+            <label htmlFor="password-input"></label>
             <input
-              id='password-input'
-              placeholder='Password'
-              className='input-field'
-              type='text'
+              id="password-input"
+              placeholder="Password"
+              className="input-field"
+              type="text"
               required
             />
           </div>
-          <p className='no-account'>Already Have an Account?<Link to="/SignIn" className='sign-up'> Sign In</Link></p>
+          <p className="no-account">
+            Already Have an Account?
+            <Link to="/SignIn" className="sign-up">
+              {" "}
+              Sign In
+            </Link>
+          </p>
 
-          <div className='useful-buttons'>
-            <button className='sign-up-button' type="submit">Sign Up</button>
-            <Link to='/'><button className='go-back-button'>Go back</button></Link>
+          <div className="useful-buttons">
+            <button className="sign-up-button" type="submit">
+              Sign Up
+            </button>
+            <Link to="/">
+              <button className="go-back-button">Go back</button>
+            </Link>
           </div>
         </form>
       </div>
-    )
-
+    );
   }
 }
 
