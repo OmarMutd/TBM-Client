ed7ca4d3d4259b3bebee6a67ef3d7b132414fe3a
Omar Muhanna <omarmuhomar@hotmail.com>
2020-05-16 09:11:36 -0500

    changes to item & sign in

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react'
 import './Item.css'
 import { Link } from 'react-router-dom';
+import config from '../../config';
 
 export default class Item extends Component {
 
-    addToCart = () => {
-        console.log('item added to cart')
-      }
-   
+    addToCart = (id) => {
+
+        fetch(`${config.API_ENDPOINT}/cart`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(data => { this.setState({ data: data }) });
+    };
+
     render() {
-        const {id, title, description, category, price, url} = this.props.product;
+        const { id, title, description, price, url } = this.props.product;
         return (
             <div className="card">
-             
-            
-                 <div className='item-card'>
-                     <div className='item-image' onClick={() => console.log('This is the image')}>
-                         <Link to={{pathname:`/SingleItem/${id}`}}><img src={url} alt={description}/></Link>
-                     </div>
-                     <button onClick={this.addToCart}>Add to Cart</button>
-                     <div className='product-information'></div>
-                      <p>{title}</p>
-                      <p>{price}</p>
-                 </div>
-            
+                <div className='item-card'>
+                    <div className='item-image'>
+                        <Link to={{ pathname: `/SingleItem/${id}` }}><img src={url} alt={description} /></Link>
+                    </div>
+                    <button onClick={() => this.addToCart(`${id}`)}>Add to Cart</button>
+                    <div className='product-information'></div>
+                    <p>{title}</p>
+                    <p>{price}</p>
+                </div>
             </div>
         )
     }
--- src/Components/SignInPage/SignInPage.js
+++ src/Components/SignInPage/SignInPage.js
@@ -6,84 +6,84 @@ import './SignInPage.css';
 
 export default class SignInPage extends Component {
 
-    static defaultProps = {
-      onLoginSuccess: () => {}
-    }
+  static defaultProps = {
+    onLoginSuccess: () => { }
+  }
 
-    state = {
-      error: null
-    }
+  state = {
+    error: null
+  }
 
-    handleSubmitJwtAuth = e => {
-      e.preventDefault()
-      this.setState({ error: null})
+  handleSubmitJwtAuth = e => {
+    e.preventDefault()
+    this.setState({ error: null })
 
-      const username = e.target.elements[0].value
-      const password = e.target.elements[1].value
+    const username = e.target.elements[0].value
+    const password = e.target.elements[1].value
 
-      AuthApiService.postLogin({
-        username: username,
-        password: password
+    AuthApiService.postLogin({
+      username: username,
+      password: password
+    })
+      .then(res => {
+
+        TokenService.saveAuthToken(res.authToken)
+        this.props.onLoginSuccess()
+
       })
-        .then(res => {
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
+  }
 
-          TokenService.saveAuthToken(res.authToken)
-          this.props.onLoginSuccess()
-          
-        })
-        .catch(res => {
-          this.setState({ error: res.error })
-        })
-    }
 
-      
-      render() {
-        const { error } = this.state
-        return (
-            <div>
-              <div role="alert">
-                {error && <p className="sign-error">{error}</p>}
-              </div>
-                <form 
-                className='signup-form'
-                onSubmit={this.handleSubmitJwtAuth}
-                >
-                    <h2 className="signup-header">Sign In</h2>
+  render() {
+    const { error } = this.state
+    return (
+      <div className='sign-in-page'>
+        <div role="alert">
+          {error && <p className="sign-error">{error}</p>}
+        </div>
+        <form
+          className='signup-form'
+          onSubmit={this.handleSubmitJwtAuth}
+        >
+          <h2 className="signup-header">Sign In</h2>
 
-                    <div className="username-signup">
-                    <label htmlFor='username-input'></label>
-                    <input
-                    id='username-input'
-                    placeholder='Username'
-                    className='input-field'
-                    type='text'
-                    required
-                    />
-                    </div>
-                    <br/>
-                    <div className="password-signup">
-                   <label htmlFor='password-input'></label>
-                    <input
-                    id='password-input'
-                    placeholder='Password'
-                    className='input-field'
-                    type='text'
-                    required
-                    />
-                   </div>
+          <div className="username-signup">
+            <label htmlFor='username-input'></label>
+            <input
+              id='username-input'
+              placeholder='Username'
+              className='input-field'
+              type='text'
+              required
+            />
+          </div>
+          <br />
+          <div className="password-signup">
+            <label htmlFor='password-input'></label>
+            <input
+              id='password-input'
+              placeholder='Password'
+              className='input-field'
+              type='text'
+              required
+            />
+          </div>
 
-                   <div className='useful-buttons'>
-                       <button typer="submit" className="sign">Sign In</button>
-                       
-                       <Link to='/'><button>Go back</button></Link>
-                       {/* <label htmlFor='signup-button'>Not a registered user? Click here to Sign up.</label> */}
-                       <Link to='/SignUpPage'><button className='signup-button'>Sign Up</button></Link>
-                   </div>
+          <div className='useful-buttons'>
+            <button type="submit" className="sign">Sign In</button>
 
-                </form>
-            </div>
-        )
-    }
+            <Link to='/'><button>Go back</button></Link>
+            {/* <label htmlFor='signup-button'>Not a registered user? Click here to Sign up.</label> */}
+            <Link to='/SignUpPage'><button className='signup-button'>Sign Up</button></Link>
+          </div>
+
+        </form>
+      </div>
+    )
+  }
 }
 
 
