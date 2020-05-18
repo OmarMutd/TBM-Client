c87f25ea6b9e583e5885f3143c1cfa25a2252f25
Omar Muhanna <omarmuhomar@hotmail.com>
2020-05-18 17:45:14 -0500

    added to cart works

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -6,12 +6,16 @@ import config from '../../config';
 export default class Item extends Component {
 
     addToCart = (id) => {
-
+        const user_id = "1"
+        const product_id = id
+        const quantity = "1"
+        const added_item = { user_id: user_id, product_id: product_id, quantity: quantity }
         fetch(`${config.API_ENDPOINT}/cart`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
+            body: JSON.stringify(added_item)
         })
             .then(response => response.json())
             .then(data => { this.setState({ data: data }) });
