118f166ff1ed0b317b54f42448de27eeb1b722e9
Laura Spieker <ljspiek@gmail.com>
2020-05-22 18:16:47 -0500

    fixed number formatting & commas in Cart.js

--- src/context.js
+++ src/context.js
@@ -1,34 +1,39 @@
-import React, { Component } from 'react';
-const ProductContext = React.createContext();
+import React, { Component } from "react";
+
+const ProductContext = React.createContext({
+  addProductToCart: () => {},
+});
 
 class ProductProvider extends Component {
-    state = {
-        cartTotal: 0,
-        cart: [],
-        productDetails: '',
-    };
+  state = {
+    cartTotal: 0,
+    cart: [],
+    productDetails: "",
+  };
 
-    addProductToCart = () => {
-       console.log('Item has been added to cart')
-    };
+  addProductToCart = () => {
+    console.log("Item has been added to cart");
+  };
 
-    itemDetails = () => {
-        console.log('Details of the item')
-    };
+  itemDetails = () => {
+    console.log("Details of the item");
+  };
 
-    render() {
-        return (
-            <div>
-                <ProductContext.Provider value={{
-                ...this.state,
-                addProductToCart: this.addProductToCart,
-                itemDetails: this.itemDetails,
-                }}>
-                    {this.props.children}
-                </ProductContext.Provider>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <ProductContext.Provider
+          value={{
+            ...this.state,
+            addProductToCart: this.addProductToCart,
+            itemDetails: this.itemDetails,
+          }}
+        >
+          {this.props.children}
+        </ProductContext.Provider>
+      </div>
+    );
+  }
 }
 
 const ProductConsumer = ProductContext.Consumer;
