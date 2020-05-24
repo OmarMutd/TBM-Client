b25c04aacc4d99e70a6214b98e449ba5fde69508
Laura Spieker <ljspiek@gmail.com>
2020-05-23 19:38:04 -0500

    passed props to test suite; all 21 tests passing now

--- src/Components/Category/Category.test.js
+++ src/Components/Category/Category.test.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Category from './Category';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import Category from "./Category";
+import { BrowserRouter } from "react-router-dom";
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render
-        (<BrowserRouter>
-            <Category />
-        </BrowserRouter>,
-            div)
-    ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <BrowserRouter>
+      <Category match={{ params: { category: "animals" } }} />
+    </BrowserRouter>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
 });
