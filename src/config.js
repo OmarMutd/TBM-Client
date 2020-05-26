9b6d2e9b56a46811a47916a325d39bce2d660cd7
Omar Muhanna <omarmuhomar@hotmail.com>
2020-05-25 19:22:46 -0500

    changed config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
   API_ENDPOINT: 'https://protected-oasis-11818.herokuapp.com/api',
-//   API_ENDPOINT: "http://localhost:8010/api",
-  TOKEN: "try2guess",
+  TOKEN_KEY: "try2guess",
 };
